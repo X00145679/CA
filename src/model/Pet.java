78354b3dbc7e63d1383bf9d9402a01e7afa5ecde
David Bird <x00148542@outlook.com>
2018-11-29 12:10:51 +0000

    Just added some stuff on the Pet class, only the basics though

--- src/model/Pet.java
+++ src/model/Pet.java
@@ -20,10 +20,70 @@ import javax.persistence.*;
 public class Pet {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "petid_seq")
+    private int petid;
     private String petname;
+    @Temporal(TemporalType.DATE)
     private String petDOB;
     private String petDatePurchased;
     private int breedid,ownid;
+    
+    public Pet() {
+    }
+
+    public Pet(String petname, String petDOB, String petDatePurchased) {
+        this.petname = petname;
+        this.petDOB = petDOB;
+        this.petDatePurchased = petDatePurchased;
+    }
+
+    public int getPetid() {
+        return petid;
+    }
+
+    public void setPetid(int petid) {
+        this.petid = petid;
+    }
+
+    public String getPetname() {
+        return petname;
+    }
+
+    public void setPetname(String petname) {
+        this.petname = petname;
+    }
+
+    public String getPetDOB() {
+        return petDOB;
+    }
+
+    public void setPetDOB(String petDOB) {
+        this.petDOB = petDOB;
+    }
+
+    public String getPetDatePurchased() {
+        return petDatePurchased;
+    }
+
+    public void setPetDatePurchased(String petDatePurchased) {
+        this.petDatePurchased = petDatePurchased;
+    }
+
+    public int getBreedid() {
+        return breedid;
+    }
+
+    public void setBreedid(int breedid) {
+        this.breedid = breedid;
+    }
+
+    public int getOwnid() {
+        return ownid;
+    }
+
+    public void setOwnid(int ownid) {
+        this.ownid = ownid;
+    }
+    
     
     
 }
