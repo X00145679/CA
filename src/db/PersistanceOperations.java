1cc663050e5f7c6e558f9db25189f1ae2acf84df
X00148542 <X00148542@computing.stu.it-tallaght.ie>
2018-11-30 13:11:43 +0000

    Started some more on PO

--- src/db/PersistanceOperations.java
+++ src/db/PersistanceOperations.java
@@ -5,10 +5,31 @@
  */
 package db;
 
+import javax.persistence.*;
+import model.Owner;
+
 /**
  *
  * @author x00148542
  */
 public class PersistanceOperations {
+    EntityManagerFactory emf;
+    EntityManager em;
     
+    public PersistanceOperations() {
+        emf = Persistence.createEntityManagerFactory("PetShopCAPU");
+        em = emf.createEntityManager();
+    }
+    
+    public void addOwner(String name, String address, String pNum){
+        em.getTransaction().begin();
+        Owner o = new Owner(name, address, pNum);
+        em.persist(o);
+        em.getTransaction().commit();
+    }
 }
+
+//String sql = "CREATE TABLE OWNER (ownid NUMBER PRIMARY KEY NOT NULL,"
+//                    + "o_name VARCHAR2(255),"
+//                    + "o_address VARCHAR2(255),"
+//                    + "o_pnum VARCHAR2(255))";
--- src/test/Test.java
+++ src/test/Test.java
@@ -15,38 +15,38 @@ public class Test {
         
         ShopOperations so = new ShopOperations();
         
-        so.openDB();
-        so.dropPetSequence();
-        so.dropBreedSequence();
-        so.dropFoodSequence();
-        so.dropOwnerSequence();
-        
-        so.createPetSequence();
-        so.createBreedSequence();
-        so.createFoodSequence();
-        so.createOwnerSequence();
-        
-        so.dropPetTable();
-        so.dropBreedTable();
-        so.dropFoodTable();
-        so.dropOwnerTable();
-        so.dropPetFoodTable();
-        
-        so.createBreedTable();
-        so.createFoodTable();
-        so.createOwnerTable();
-        so.createPetTable();
-        so.createPetFoodTable();
-        
-        so.fillBreedTable();
-        so.fillFoodTable();
-        so.fillOwnerTable();
-        so.fillPetTable();
+//        so.openDB();
+//        so.dropPetSequence();
+//        so.dropBreedSequence();
+//        so.dropFoodSequence();
+//        so.dropOwnerSequence();
+//        
+//
+//        so.createBreedSequence();
+//        so.createFoodSequence();
+//        so.createOwnerSequence();
+//        
+//        so.dropPetTable();
+//        so.dropBreedTable();
+//        so.dropFoodTable();
+//        so.dropOwnerTable();
+//        so.dropPetFoodTable();
+//        
+//        so.createBreedTable();
+//        so.createFoodTable();
+//        so.createOwnerTable();
+//        so.createPetTable();
+//        so.createPetFoodTable();
+//        
+//        so.fillBreedTable();
+//        so.fillFoodTable();
+//        so.fillOwnerTable();
+//        so.fillPetTable();
         
         PersistanceOperations po = new PersistanceOperations();
         
-        while (true) {
-            System.out.println("Please press 1 to quit");
+        
+            System.out.println("Please press 1 to add new owner");
             System.out.println("Please press 2 to Update");
             System.out.println("Please press 3 to delete");
             System.out.println("Please press 4 to Query");
@@ -56,12 +56,19 @@ public class Test {
             
             switch (choice) {
                 case 1:
-                    so.closeDB();
-                    System.exit(0);
+                    System.out.println("Please enter the name of the owner "
+                            + "you wish to add");
+                    String name = in.nextLine();
+                    System.out.println("Please enter the address of the owner "
+                            + "you wish to add");
+                    String address = in.nextLine();
+                    System.out.println("Please enter the phone number of the owner "
+                            + "you wish to add");
+                    String pNum = in.nextLine();
                     break;
                 default:
                     System.out.println("Invalid Option entered");
             }
-        }
+        
     }
 }
